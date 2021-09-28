799869256cb93aadba451b2ffda4b3f00880cf7b
Abdullah Koroglu <abdullah34koroglu@gmail.com>
2021-09-28 18:54:56 +0300

    episodes

--- src/pages/User.js
+++ src/pages/User.js
@@ -3,13 +3,21 @@ import React, { useEffect, useState } from "react";
 export default (props) => {
   const [id, setId ] = useState([])
   const [data, setData] = useState([])
+  const [episodes, setEpisodes] = useState([])
   useEffect(()=>{
       setId(props.match.params.user)
       fetch('https://rickandmortyapi.com/api/character/'+props.match.params.user)
             .then(response => response.json())
-            .then(res => {
+            .then(async(res) => {
                 console.log(res);
                 setData(res)
+                let episodes = res.episode
+                episodes.slice(-5).map(async(link)=>{
+                    let response = await fetch(link)
+                    let res = await response.json()
+                    console.log(res);
+                    setEpisodes(prev => [...prev , res])
+                })
             });
 },[])
     console.log(props.match.params.user);
@@ -18,6 +26,11 @@ export default (props) => {
                     <img src={data.image} alt={data.name} width="500" height="600" />
                     <p>{data.name}</p>
                     <p>{data.location?.name}</p>
+                    {episodes.map(i=>{
+                        return <div>
+                            {i.name}
+                        </div>
+                    })}
 
     </div>
   );
